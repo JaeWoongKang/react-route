37f2bdcb2f29fa8c0d73b889638151119a97578b
JaeWoongKang <kangwodnd@gmail.com>
2018-05-10 08:19:09 +0900

    react-router : Link 와 match 실습하기

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route } from "react-router-dom";
+import { Route, Link } from "react-router-dom";
 const App = () => (
   <div>
     <Header />
@@ -9,6 +9,12 @@ const App = () => (
 const Header = () => (
   <header>
     <h1>My Contacts</h1>
+    <nav>
+      <ul>
+        <li><Link to ="/">HOME</Link></li>
+        <li><Link to="/contacts">CONTACT</Link></li>
+      </ul>
+    </nav>
     <Route exact path="/" component={Welcome}/>
     <Route path="/contacts" component={Contacts}/>
   </header> 
@@ -20,12 +26,22 @@ const Welcome = () => (
   </h1>
 )
 
-const Contacts = () => (
-  <ul>
-    <li>Jisu</li>
-    <li>Jaewoong</li>
-    <li>Yoonhee</li>
-  </ul>
+//match 은 라우터가 주는 prop이다.
+//내가 어디에있는지 정보를 줌 나의 컨택 안에서 새로운 라우터 생성을 원함
+const Contacts = ({match}) => (
+  <div>
+    <ul>
+      <li><Link to="/contacts/jisu">Jisu</Link></li>
+      <li><Link to="/contacts/jaewoong">Jaewoong</Link></li>
+      <li><Link to="/contacts/yoonhee">Yoonhee</Link></li>
+    </ul>
+    <Route exact path={`${match.path}`} render={()=> <h3>Please select a contact</h3>}/>
+    <Route path={`${match.path}/:contactName`} component={Contact}/>
+  </div>
 )
+
+const Contact = ({match}) =>
+  `Your friend name is ${match.params.contactName}`;
+
 
 export default App;
